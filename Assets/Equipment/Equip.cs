5f760323a0bf05cea9f0db395580e98b87554210
Tobias <tobias.manger@gmail.com>
2019-01-25 20:39:13 +0100

    equipment 0.1

---
+++ Assets/Equipment/Equip.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Equip : MonoBehaviour
+{
+    public Transform rightHand;
+    public Equipment equipment;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GameObject go = Instantiate(equipment.Prefab, rightHand);
+            go.transform.localPosition = equipment.Position;
+            go.transform.localEulerAngles = equipment.Rotation; 
+        }
+
+    }
+}
---
+++ Assets/Scripts/Equipment.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+/* The base item class. All items should derive from this. */
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Equipment")]
+public class Equipment : Item {
+
+    public GameObject Prefab;
+    public Vector3 Position;
+    public Vector3 Rotation;
+}
