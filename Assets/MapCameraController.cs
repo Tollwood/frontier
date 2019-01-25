e1a0d8defef01b27992325b3e4bbc51772cc0291
Tobias <tobias.manger@gmail.com>
2019-01-25 20:56:12 +0100

    play with map

---
+++ Assets/MapCameraController.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+[RequireComponent(typeof( Camera))]
+public class MapCameraController : MonoBehaviour
+{
+    public float minFov = 15f;
+    public float maxFov = 90f;
+    public float sensitivity = 10f;
+
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    public void Update()
+    {
+        float fov =cam.fieldOfView;
+        fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        fov = Mathf.Clamp(fov, minFov, maxFov);
+        cam.fieldOfView = fov;
+    }
+}
